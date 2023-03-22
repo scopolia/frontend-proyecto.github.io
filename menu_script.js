8f53d7f6df9347e932640d294b646a012ac53523
scopolia <126039257+scopolia@users.noreply.github.com>
2023-03-22 13:53:06 -0300

    Add files via upload

---
+++ menu_script.js
@@ -0,0 +1,11 @@
+
+    document.getElementById('linkForm').onmouseover  = function(){
+    document.getElementById('linkForm').innerHTML= "click aqui"};
+
+    document.getElementById('linkForm').onmouseout = function(){
+    document.getElementById('linkForm').innerHTML= "Contacto"};
+
+  
+  
+
+
